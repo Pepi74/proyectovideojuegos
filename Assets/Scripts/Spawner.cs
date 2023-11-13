01471515ab3072a80fc9f5b53a1d136a1619fb01
Pepi74 <pipexsonic@hotmail.com>
2023-11-13 16:16:38 -0300

    Agregado paredes invisibles al borde del terreno

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,6 +9,7 @@ public class Spawner : MonoBehaviour
     public GameObject playerPrefab;
     public Terrain terrain; // Referencia al terreno
     public PlayerSpawnEvent onPlayerSpawned = new PlayerSpawnEvent();
+    public GameObject boundaryPrefab;
 
     public int minSpawn = 100; // Cantidad minima de huevos que spawnearan
     public int maxSpawn = 200; // Cantidad maxima de huevos que spawnearan
@@ -16,6 +17,7 @@ public class Spawner : MonoBehaviour
     // Manejo de spawn de huevos alrededor del terreno y player en el centro del terreno
     void Start()
     {
+        SpawnBoundaries();
         SpawnPlayer();
         SpawnEggs();
     }
@@ -26,8 +28,8 @@ public class Spawner : MonoBehaviour
 
         for (int i = 0; i < Random.Range(minSpawn, maxSpawn+1); i++)
         {
-            float randomX = Random.Range(0f, terrainData.size.x);
-            float randomZ = Random.Range(0f, terrainData.size.z);
+            float randomX = Random.Range(0f, terrainData.size.x - 2f);
+            float randomZ = Random.Range(0f, terrainData.size.z - 2f);
 
             float terrainHeight = terrain.SampleHeight(new Vector3(randomX, 0, randomZ));
 
@@ -46,5 +48,33 @@ public class Spawner : MonoBehaviour
         Vector3 spawnPosition = new Vector3(spawnX, terrainHeight + 2, spawnZ);
         GameObject player = Instantiate(playerPrefab, spawnPosition, Quaternion.identity);
         onPlayerSpawned.Invoke(player);
+    }
+
+    void SpawnBoundaries()
+    {
+        TerrainData terrainData = terrain.terrainData;
+        Vector3 terrainSize = terrain.terrainData.size;
+
+        float[] terrainHeights = {
+            terrain.SampleHeight(new Vector3(0, 0, terrainSize.z / 2)),
+            terrain.SampleHeight(new Vector3(terrainSize.x, 0, terrainSize.z / 2)),
+            terrain.SampleHeight(new Vector3(terrainSize.x / 2, 0, 0)),
+            terrain.SampleHeight(new Vector3(terrainSize.x / 2, 0, terrainSize.z))
+        };
+
+        Vector3[] boundaryPositions = {
+            new Vector3(0, terrainHeights[0], terrainSize.z / 2), // Right side
+            new Vector3(terrainSize.x, terrainHeights[1], terrainSize.z / 2), // Left side
+            new Vector3(terrainSize.x / 2, terrainHeights[2], 0), // Top side
+            new Vector3(terrainSize.x / 2, terrainHeights[3], terrainSize.z) // Bottom side
+        };
+
+        foreach(Vector3 position in boundaryPositions)
+        {
+            GameObject boundary = Instantiate(boundaryPrefab, position, Quaternion.identity);
+            if (position.z != terrainSize.z / 2) boundary.transform.localScale = new Vector3(terrainSize.x, 50f, 4);
+            else
+                boundary.transform.localScale = new Vector3(4, 50f, terrainSize.z);
+        }
     }
 }
