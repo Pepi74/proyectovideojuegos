b43a93f1b6a275426a80fee359eff416782382ba
Pepi74 <pipexsonic@hotmail.com>
2023-11-20 22:38:50 -0300

    Modificaciones hechas
    * Lily pad es un prefab
    * Se elimina EggSpawner ya que no se usa
    * LilySpawner y TreeSpawner se movieron a Spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -14,19 +14,28 @@ public class Spawner : MonoBehaviour
     public int minSpawn = 100; // Cantidad minima de huevos que spawnearan
     public int maxSpawn = 200; // Cantidad maxima de huevos que spawnearan
 
+    public GameObject water;
+    public GameObject lilyPrefab;
+    public int MaxPads = 200;
+
+    public GameObject treePrefab; // The tree prefab you want to add
+    public int numberOfTrees = 100;
+
     // Manejo de spawn de huevos alrededor del terreno y player en el centro del terreno
     void Start()
     {
         SpawnBoundaries();
         SpawnPlayer();
         SpawnEggs();
+        SpawnLilyPads();
+        SpawnTrees();
     }
 
     void SpawnEggs()
     {
         TerrainData terrainData = terrain.terrainData;
 
-        for (int i = 0; i < Random.Range(minSpawn, maxSpawn+1); i++)
+        for (int i = 0; i < Random.Range(minSpawn, maxSpawn + 1); i++)
         {
             float randomX = Random.Range(0f, terrainData.size.x - 2f);
             float randomZ = Random.Range(0f, terrainData.size.z - 2f);
@@ -55,21 +64,23 @@ public class Spawner : MonoBehaviour
         TerrainData terrainData = terrain.terrainData;
         Vector3 terrainSize = terrain.terrainData.size;
 
-        float[] terrainHeights = {
+        float[] terrainHeights =
+        {
             terrain.SampleHeight(new Vector3(0, 0, terrainSize.z / 2)),
             terrain.SampleHeight(new Vector3(terrainSize.x, 0, terrainSize.z / 2)),
             terrain.SampleHeight(new Vector3(terrainSize.x / 2, 0, 0)),
             terrain.SampleHeight(new Vector3(terrainSize.x / 2, 0, terrainSize.z))
         };
 
-        Vector3[] boundaryPositions = {
+        Vector3[] boundaryPositions =
+        {
             new Vector3(0, terrainHeights[0], terrainSize.z / 2), // Right side
             new Vector3(terrainSize.x, terrainHeights[1], terrainSize.z / 2), // Left side
             new Vector3(terrainSize.x / 2, terrainHeights[2], 0), // Top side
             new Vector3(terrainSize.x / 2, terrainHeights[3], terrainSize.z) // Bottom side
         };
 
-        foreach(Vector3 position in boundaryPositions)
+        foreach (Vector3 position in boundaryPositions)
         {
             GameObject boundary = Instantiate(boundaryPrefab, position, Quaternion.identity);
             if (position.z != terrainSize.z / 2) boundary.transform.localScale = new Vector3(terrainSize.x, 50f, 4);
@@ -77,4 +88,58 @@ public class Spawner : MonoBehaviour
                 boundary.transform.localScale = new Vector3(4, 50f, terrainSize.z);
         }
     }
+
+    void SpawnLilyPads()
+    {
+        Renderer waterRenderer = water.GetComponent<Renderer>();
+        // Get the bounds of the water in world space
+        Bounds waterBounds = waterRenderer.bounds;
+        // Get the X and Z coordinates of the edges
+        float minX = waterBounds.min.x;
+        float maxX = waterBounds.max.x;
+
+        float minZ = waterBounds.min.z;
+        float maxZ = waterBounds.max.z;
+
+        float randomPosX;
+        float randomPosZ;
+
+        for (int i = 0; i < MaxPads; i++)
+        {
+            randomPosX = Random.Range(minX, maxX);
+            randomPosZ = Random.Range(minZ, maxZ);
+            Instantiate(lilyPrefab, new Vector3(randomPosX, water.transform.position.y + 2f, randomPosZ),
+                Quaternion.identity);
+        }
+    }
+
+    void SpawnTrees()
+    {
+        if (terrain == null || treePrefab == null)
+        {
+            Debug.LogError("Terrain or treePrefab not assigned!");
+            return;
+        }
+
+        AddTree();
+    }
+
+    void AddTree()
+    {
+        TerrainData terrainData = terrain.terrainData;
+
+        for (int i = 0; i < numberOfTrees; i++)
+        {
+
+            // Choose a position for the tree within the terrain size
+            float randomX = Random.Range(0f, terrainData.size.x);
+            float randomZ = Random.Range(0f, terrainData.size.z);
+
+            float terrainHeight = terrain.SampleHeight(new Vector3(randomX, 0, randomZ));
+
+            Vector3 spawnPosition = new Vector3(randomX, terrainHeight + 0.5f, randomZ);
+            Instantiate(treePrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
 }
