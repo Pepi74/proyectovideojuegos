9bb8e0251e81993f3c9b80ed3bc99fad3dfb7b31
Pepi74 <pipexsonic@hotmail.com>
2023-11-20 22:56:08 -0300

    animaciones y scripts agregados

---
+++ Assets/Scripts/Tounge.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tounge : MonoBehaviour
+{
+    public bool canAttack = true; //Variable general que permite al jugador atacar
+
+    public Transform toungeBase, cam; //Transform de la base de la lengua en el player y de la camara
+    public float toungeRange; //Rango de la lengua (por ahora no se usa)
+    public float toungeDelayTime; // delay para que la lengua llegue e su objetivo (por ahora no se usa)
+    public float fireRate; // ratio de disparo de la lengua
+    public float fireTimer; // variable de conteo para el ratio de fuego.
+    public GameObject toungePrefab; //prefab de la punta de la lengua
+    private Transform toungeTip; //prefab de la punta de la lengua
+    
+    public float meleeeRange; //Rango de ataque melee
+    public int meleeDamage; // daño de melee
+    public float meleeFireRate; // ratio de ataque melee
+    public float meleeTimer; // variable de conteo para el ataque melee.
+
+    private void Start()
+    {
+        fireTimer = 0; //inicializar timer
+    }
+
+    private void Update()
+    {
+        //Iniciar ataque a rango
+        if (Input.GetMouseButton(0) && fireTimer <= 0f && canAttack)
+        {
+            fireTimer = 1 / fireRate;
+            meleeTimer = 0.3f;
+            Attack();
+        }
+        //Iniciar ataque Melee
+        if (Input.GetMouseButton(1) && meleeTimer <= 0f && canAttack)
+        {
+            fireTimer = 0.3f;
+            meleeTimer = 1 / meleeFireRate;
+            meleeAttack();
+        }
+
+        //Contador de ratio de fuego
+        if (fireTimer > 0) fireTimer -= Time.deltaTime;
+
+        
+
+    }
+   
+    //funcion de ataque de rango
+    public void Attack()
+    {
+        //iniciar el ataque
+        GameObject bullet = Instantiate(toungePrefab, toungeBase.position, cam.rotation);
+        //raycast para el ataque
+        RaycastHit hit;
+        if (Physics.Raycast(cam.position,cam.forward,out hit,10f))
+        {
+            bullet.transform.LookAt(hit.point);
+        }
+    }
+
+    public void meleeAttack()
+    {
+        foreach (Collider other in Physics.OverlapSphere(toungeBase.position, meleeeRange))
+        {
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                EnemyScript enemyScript = other.gameObject.GetComponent<EnemyScript>();
+                enemyScript.TakeDamage(meleeDamage);
+            }
+        }
+    }
+}
